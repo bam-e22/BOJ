6bf708ab189d99da4c34ce7360585e24a707214d
stack07142 <stack.07142@gmail.com>
2016-10-24 02:38:25 +0900

    BOJ#1001_A-B

---
+++ BOJ#1001_A-B/src/Main.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/*
+ * https://www.acmicpc.net/problem/1001
+ * A-B
+ */
+public class Main {
+
+	public static void main(String[] args) {
+
+		try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+
+			System.out.println(Integer.parseInt(st.nextToken()) - Integer.parseInt(st.nextToken()));
+
+		} catch (IOException e) {
+
+			e.printStackTrace();
+
+		}
+	} // main
+}
