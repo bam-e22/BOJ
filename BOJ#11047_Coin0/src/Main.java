b004c6f8c434590319e9d796f537cbf1eb17ea46
stack07142 <stack.07142@gmail.com>
2016-11-10 01:02:51 +0900

    BOJ#11047_Coun0

---
+++ BOJ#11047_Coin0/src/Main.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+
+		int N;
+		int K;
+		int[] A;
+		int count = 0;
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		N = Integer.parseInt(st.nextToken());
+		K = Integer.parseInt(st.nextToken());
+
+		A = new int[N];
+		for (int i = 0; i < N; i++) {
+
+			A[i] = Integer.parseInt(br.readLine());
+		}
+
+		for (int i = N - 1; i >= 0; i--) {
+
+			if (A[i] <= K) {
+				count += (K / A[i]);
+				K %= A[i];
+			}
+
+		}
+		System.out.println(count);
+
+	}
+
+}
