2178001c16e72e8da9d40968d90a0e6fd4b235f5
stack07142 <stack.07142@gmail.com>
2017-02-06 22:03:11 +0900

    BOJ

---
+++ BOJ#10998_AB/src/Main.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        int A, B;
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        A = Integer.parseInt(st.nextToken());
+        B = Integer.parseInt(st.nextToken());
+
+        System.out.println(A * B);
+
+    }
+}
