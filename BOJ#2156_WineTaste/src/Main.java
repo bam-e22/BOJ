c4c2827c006f31856905bb1dfbf14a2f8771c44c
stack07142 <stack.07142@gmail.com>
2017-03-13 16:03:13 +0900

    BOJ

--- BOJ#2156_WineTaste/src/Main.java
+++ BOJ#2156_WineTaste/src/Main.java
@@ -2,57 +2,59 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-/* 
+/**
  * BOJ#2156 포도주 시식
  * https://www.acmicpc.net/problem/2156
  */
 
 public class Main {
 
-	static int[] dp = new int[10001];
+    public static void main(String[] args) throws IOException {
 
-	public static void main(String[] args) throws IOException {
+        int n;
+        int[] wine; // 포도주의 양
+        int[] dp; // dp[n] : n잔까지 마셨을 때, 최대로 마실 수 있는 포도주의 양
 
-		int n;
-		int[] p;
+        // Input
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		// Input
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
 
-		n = Integer.parseInt(br.readLine());
-		p = new int[n + 1];
+        wine = new int[n + 1];
+        dp = new int[n + 1];
 
-		for (int i = 1; i <= n; i++) {
+        for (int i = 1; i <= n; i++) {
 
-			p[i] = Integer.parseInt(br.readLine());
-		}
+            wine[i] = Integer.parseInt(br.readLine());
+        }
 
-		// dp[n]
-		System.out.println(go(n, p));
+        // dp[n]
+        System.out.println(solve(wine, dp, n));
 
-	}
+    }
 
-	static int go(int n, int[] p) {
+    static int solve(int[] wine, int[] dp, int n) {
 
-		if (n < 1) {
+        if (n < 1) {
 
-			return 0;
-		} else if (n == 1) {
+            return 0;
+        } else if (n == 1) {
 
-			return p[1];
-		} else if (n == 2) {
+            return wine[1];
+        } else if (n == 2) {
 
-			return p[1] + p[2];
-		}
+            return wine[1] + wine[2];
+        }
 
-		if (dp[n] > 0) {
+        // memoization
+        if (dp[n] > 0) {
 
-			return dp[n];
-		}
+            return dp[n];
+        }
 
-		dp[n] = Math.max(Math.max(go(n - 1, p), go(n - 2, p) + p[n]), go(n - 3, p) + p[n] + p[n - 1]);
+        // dynamic programming
+        dp[n] = Math.max(Math.max(solve(wine, dp, n - 1), solve(wine, dp, n - 2) + wine[n]), solve(wine, dp, n - 3) + wine[n] + wine[n - 1]);
 
-		return dp[n];
-	}
-
+        return dp[n];
+    }
 }
