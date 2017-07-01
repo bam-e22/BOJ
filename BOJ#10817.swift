c971bc5a455e1b00166f2ef525cf652fa6ff0365
stack07142 <stack.07142@gmail.com>
2017-07-01 20:08:18 +0900

    apply .gitignore

--- BOJ#10817.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-
-let input = readLine()!
-var inputArr = input.characters.split(separator: " ").map({Int(String($0))!})
-
-inputArr = inputArr.sorted()
-print(inputArr[1])
-
--- BOJ#3046.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-
-let inputArr = readLine()!.characters.split(separator: " ").map({Int(String($0))!})
-
-let R1 : Int = inputArr[0]
-let S : Int = inputArr[1]
-
-print(2 * inputArr[1] - inputArr[0])
